9e796442e1c3171e8cfd8aa25a12e8ecf7ffcc67
Matej <matej.galic.ets@gmail.com>
2023-01-21 13:45:26 +0100

    logout button added and functionality login/logout added

--- js/show_nastavni_plan.js
+++ js/show_nastavni_plan.js
@@ -1,9 +1,18 @@
 window.onload = function () {
     var nastavni_plan = $("#nastavni_plan_hidden");
+    var login_btn = $("#login_btn");
+    var logout_btn = $("#logout_btn");
 
     if (localStorage.hasOwnProperty('token')) {
         nastavni_plan.show();
+        login_btn.hide();
+        logout_btn.show();
     } else {
         nastavni_plan.hide();
     }
+
+    logout_btn.click(function () {
+        localStorage.clear();
+        location.reload();
+    });
 }
